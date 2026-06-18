ea8fe59a72f1f2d8aa818f6500fa9acaf605873f
Poojaa <salagavapoojasri@gmail.com>
2026-06-18 13:44:56 +0530

    Initial backend setup

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "ReStyle Backend Running"}
+
+@app.get("/test-db")
+def test_db():
+    try:
+        connection = engine.connect()
+        connection.close()
+        return {"message": "Database Connected Successfully"}
+    except Exception as e:
+        return {"error": str(e)}
